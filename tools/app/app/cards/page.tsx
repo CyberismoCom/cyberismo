543ec1ee8dc3abc1ebb4c4e9c6b1a4e48ec8ae47
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-06-13 12:11:52 +0300

    INTDEV-303 display different text for user when project is empty

--- tools/app/app/cards/page.tsx
+++ tools/app/app/cards/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import { useProject } from '../lib/api'
 
 export const dynamic = 'force-dynamic'
 
 export default function CardsPage() {
   const { t } = useTranslation()
-  return <Typography variant="h6">{t('selectCard')}</Typography>
+  const { project } = useProject()
+  return <Typography variant="h6">{project && project.cards.length > 0 ? t('selectCard') : t('emptyProject')}</Typography>
 }
