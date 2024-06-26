59445b633cded1f8b43793bf37e2a356b61e56c1
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-06-26 13:13:45 +0300

    Add unsaved file

--- tools/app/app/lib/hooks/redux.ts
+++ tools/app/app/lib/hooks/redux.ts
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector, useStore } from 'react-redux'
-import type { RootState, AppDispatch, AppStore } from './store'
+import type { RootState, AppDispatch, AppStore } from '../store'
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector = useSelector.withTypes<RootState>()
