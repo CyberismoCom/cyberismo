2bd5b9e376dc8495db5d425cbedf534560f61a8f
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-06-28 16:10:00 +0300

    Converted StateSelector to joy ui

--- tools/app/app/components/StateSelector.tsx
+++ tools/app/app/components/StateSelector.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useMemo } from 'react'
 import colors from '@mui/joy/colors'
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
 import { Workflow, WorkflowState, WorkflowTransition } from '../lib/definitions'
 import { workflowCategory } from '../../../data-handler/src/interfaces/project-interfaces'
-import { Menu, MenuItem, ListItemContent, Button } from '@mui/joy'
+import { Menu, MenuItem, ListItemContent, Dropdown, MenuButton } from '@mui/joy'
 
 interface StateSelectorProps {
   currentState: WorkflowState | null
@@ -16,63 +16,43 @@ const StateSelector: React.FC<StateSelectorProps> = ({
   workflow,
   onTransition,
 }) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
   const handleMenuItemClick = (transition: WorkflowTransition) => {
     onTransition(transition)
-    handleClose()
   }
 
-  if (
-    currentState == null ||
-    workflow == null ||
-    !workflow.states.find((state) => state.name == currentState.name)
-  )
-    return null
+  const availableTransitions = useMemo(() => {
+    if (
+      !currentState ||
+      !workflow ||
+      !workflow.states.find((state) => state.name == currentState.name)
+    ) {
+      return []
+    }
 
-  const availableTransitions = workflow?.transitions.filter(
-    (transition) =>
-      transition.fromState.includes(currentState.name) ||
-      transition.fromState.includes('*')
-  )
+    return workflow?.transitions.filter(
+      (transition) =>
+        transition.fromState.includes(currentState.name) ||
+        transition.fromState.includes('*')
+    )
+  }, [currentState, workflow])
+
+  if (!availableTransitions || !currentState) return null
 
   return (
-    <div>
-      <Button
+    <Dropdown>
+      <MenuButton
+        size="sm"
         disabled={availableTransitions.length == 0}
         variant="soft"
-        onClick={handleClick}
-        color="neutral"
-        size="sm"
         startDecorator={
           <FiberManualRecordIcon
             style={{ color: getStateColor(currentState) }}
           />
         }
-        sx={{
-          pl: 2,
-          pr: 2,
-          marginLeft: 1,
-        }}
       >
         <strong>Status: {currentState.name}</strong>
-      </Button>
-      <Menu
-        id="status-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={open}
-        onClose={handleClose}
-      >
+      </MenuButton>
+      <Menu>
         {availableTransitions.map((transition) => (
           <MenuItem
             key={transition.name}
@@ -82,7 +62,7 @@ const StateSelector: React.FC<StateSelectorProps> = ({
           </MenuItem>
         ))}
       </Menu>
-    </div>
+    </Dropdown>
   )
 }
 
