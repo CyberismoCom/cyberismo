ed0ce4f67d7f0537b36e4bbe653b8ab932c77e06
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-07-04 14:25:18 +0300

    Add eslint-config-prettier

--- tools/data-handler/eslint.config.js
+++ tools/data-handler/eslint.config.js
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
@@ -9,4 +10,5 @@ export default [
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
