704bde1e12ac3fb88c9bedd9306d2421a3949c74
Sami Lempinen <sami.lempinen@cyberismo.com>
2024-10-13 10:46:27 +0300

    Fix prettier warnings

--- tools/data-handler/src/command-handler.ts
+++ tools/data-handler/src/command-handler.ts
@@ -1208,7 +1208,8 @@ export class Commands {
           `Starting Cyberismo on http://localhost:${port}. Enter CTRL+C to quit.`,
         );
         execFileSync(`npm`, args, { shell: true, cwd: `${appPath}` });
-      } catch (error) { // eslint-disable-line @typescript-eslint/no-unused-vars
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
         port++;
         args = [
           `start`,
