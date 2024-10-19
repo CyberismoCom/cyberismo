265b43acfc51f3d10decf42aa41014478fd07c22
Sami Merilä <sami.merila@cyberismo.com>
2024-10-19 15:41:58 +0300

    Add missing copyright blocks

--- tools/data-handler/src/utils/random.ts
+++ tools/data-handler/src/utils/random.ts
@@ -1,4 +1,16 @@
 /**
+    Cyberismo
+    Copyright © Cyberismo Ltd and contributors 2024
+
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/**
  * Returns a random string in given base with given length (padded with 0's if necessary)
  * @param {number} base which base to be used, e.g. 36 for base-36, must be an integer
  * @param {number} length length of string, must be an integer
