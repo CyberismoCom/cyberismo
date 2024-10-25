a2a2821b4a6cf36fb795f1c8914b3d0f9b3ddc48
Sami Merilä <sami.merila@cyberismo.com>
2024-10-25 09:12:39 +0300

    INTDEV-525 Remove stdout/stderr prints from export-site unit tests (#220)

--- tools/data-handler/src/export-site.ts
+++ tools/data-handler/src/export-site.ts
@@ -28,6 +28,10 @@ import { Export } from './export.js';
 import { Project } from './containers/project.js';
 import { sortItems } from './utils/lexorank.js';
 
+interface ExportOptions {
+  silent: boolean;
+}
+
 export class ExportSite extends Export {
   private tmpDir: string = '';
   private moduleDir: string = '';
@@ -36,6 +40,7 @@ export class ExportSite extends Export {
   private playbookDir: string = '';
   private playbookFile: string = '';
   private navFile: string = '';
+  private options: ExportOptions | undefined;
 
   // todo: change this so that split temp folder creation to its own method.
   // then parallelize this and export() as much as you can.
@@ -67,9 +72,13 @@ export class ExportSite extends Export {
 
   // Generate the site from the source files using Antora.
   private generate(outputPath: string) {
-    // Use spawnsync to npx execute the program "antora", with the argument this.playbookFile
+    const additionalArguments = ['--to-dir', outputPath, this.playbookFile];
+    if (this.options && this.options?.silent) {
+      additionalArguments.unshift('--silent');
+    }
+    // Use spawnsync to npx execute the program "antora"
     try {
-      spawnSync('npx', ['antora', '--to-dir', outputPath, this.playbookFile], {
+      spawnSync('npx', ['antora', ...additionalArguments], {
         stdio: 'inherit',
       });
     } catch (error) {
@@ -247,7 +256,9 @@ export class ExportSite extends Export {
     source: string,
     destination: string,
     cardKey?: string,
+    options?: ExportOptions,
   ) {
+    this.options = options;
     Export.project = new Project(source);
     const sourcePath: string = cardKey
       ? join(
