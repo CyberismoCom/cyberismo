728a0e0a133e9411d0efa2e4dc4d76b7af43a354
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-11-26 16:22:29 +0200

    Add delay to test

--- tools/app/cypress/e2e/app.cy.ts
+++ tools/app/cypress/e2e/app.cy.ts
@@ -206,12 +206,14 @@ describe('Navigation', () => {
     cy.get('.MuiAutocomplete-option').contains('Untitled page').click(); // Select Untitled page
     cy.get('button').contains('Add link').click(); // Click add link button
 
+    cy.wait(1000);
     cy.get('[data-cy="cardLinkType"]').contains('blocks');
     cy.get('[data-cy="cardLinkTitle"]').contains('Untitled page');
 
     cy.get(':nth-child(5) > :nth-child(2)').should('not.exist'); // checks 2nd link was not created
 
     cy.get('[data-cy="cardLink"]').click(); // Click created card link
+    cy.wait(1000);
 
     // Verifies link exists in Untitled page
     cy.get('h1').contains('Untitled page');
