f0d7444f297bc7f3e9ca7103cde2dd66110e68a2
Jaakko Koskenkorva <jaakko@11latoa.com>
2024-11-28 17:37:23 +0200

    Clone cli&app test content modules to .tmp folder
    Also create test projects there. Add .tmp to gitignore and prettierignore.

--- tools/cli/test/cli.test.ts
+++ tools/cli/test/cli.test.ts
@@ -8,11 +8,11 @@ import { existsSync, rmSync } from 'fs';
 
 // CI should clone the module-base and cyberismo-docs repositories to these paths
 // Locally run test clone the repositories to these paths in before hook
-const baseModulePath = '../../module-base';
-const docsPath = '../../cyberismo-docs';
+const baseModulePath = '../../.tmp/module-base';
+const docsPath = '../../.tmp/cyberismo-docs';
 
 // Path for the test project that is created during tests
-const cliPath = '../../cyberismo-cli';
+const cliPath = '../../.tmp/cyberismo-cli';
 
 let pageCardKey = '';
 let decisionCardKey = '';
@@ -25,12 +25,12 @@ describe('Cli BAT test', function () {
   before(() => {
     if (!existsSync(baseModulePath)) {
       execSync(
-        'cd ../../&&git clone git@github.com:CyberismoCom/module-base.git',
+        'cd ../../&&git clone git@github.com:CyberismoCom/module-base.git .tmp/module-base',
       );
     }
     if (!existsSync(docsPath)) {
       execSync(
-        'cd ../../&&git clone git@github.com:CyberismoCom/cyberismo-docs.git',
+        'cd ../../&&git clone git@github.com:CyberismoCom/cyberismo-docs.git .tmp/cyberismo-docs',
       );
     }
   });
@@ -40,7 +40,7 @@ describe('Cli BAT test', function () {
   });
   it('validate module-base', function (done) {
     exec(
-      'cd ../../module-base&&cyberismo validate ',
+      'cd ../../.tmp/module-base&&cyberismo validate ',
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -53,7 +53,7 @@ describe('Cli BAT test', function () {
   });
   it('validate cyberismo-docs', function (done) {
     exec(
-      'cd ../../cyberismo-docs&&cyberismo validate ',
+      'cd ../../.tmp/cyberismo-docs&&cyberismo validate ',
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -66,7 +66,7 @@ describe('Cli BAT test', function () {
   });
   it('Create and validate new project', function (done) {
     exec(
-      'cd ../../&&cyberismo create project "CLI Basic Acceptance Test" bat cyberismo-cli&&cd cyberismo-cli&&cyberismo validate',
+      'cd ../../.tmp&&cyberismo create project "CLI Basic Acceptance Test" bat cyberismo-cli&&cd cyberismo-cli&&cyberismo validate',
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -79,7 +79,7 @@ describe('Cli BAT test', function () {
   });
   it('Import module-base', function (done) {
     exec(
-      'cd ../../cyberismo-cli&&cyberismo import module ../module-base&&cyberismo validate',
+      'cd ../../.tmp/cyberismo-cli&&cyberismo import module ../module-base&&cyberismo validate',
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -92,7 +92,7 @@ describe('Cli BAT test', function () {
   });
   it('Create a page', function (done) {
     exec(
-      'cd ../../cyberismo-cli&&cyberismo create card base/templates/page&&cyberismo validate',
+      'cd ../../.tmp/cyberismo-cli&&cyberismo create card base/templates/page&&cyberismo validate',
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -107,7 +107,7 @@ describe('Cli BAT test', function () {
   });
   it('Create a decision as a child of the page', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create card base/templates/decision ${pageCardKey}&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create card base/templates/decision ${pageCardKey}&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -122,7 +122,7 @@ describe('Cli BAT test', function () {
   });
   it('Approve the decision in its workflow', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo transition ${decisionCardKey} Approve&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo transition ${decisionCardKey} Approve&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -136,7 +136,7 @@ describe('Cli BAT test', function () {
   });
   it('Create a new workflow', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create workflow workflowTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create workflow workflowTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -150,7 +150,7 @@ describe('Cli BAT test', function () {
   });
   it('Create a new cardtype that uses the new workflow', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create cardType cardTypeTest workflowTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create cardType cardTypeTest workflowTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -164,7 +164,7 @@ describe('Cli BAT test', function () {
   });
   it('Create a new fieldtype', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create fieldType fieldTypeTest number&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create fieldType fieldTypeTest number&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -178,7 +178,7 @@ describe('Cli BAT test', function () {
   });
   it('Create a new linktype', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create linkType linkTypeTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create linkType linkTypeTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -192,7 +192,7 @@ describe('Cli BAT test', function () {
   });
   it('Create a new template', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create template templateTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create template templateTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -206,7 +206,7 @@ describe('Cli BAT test', function () {
   });
   it('Add a card of the new cardtype to the newly created template', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo add base/templates/templateTest bat/cardTypes/cardTypeTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo add base/templates/templateTest bat/cardTypes/cardTypeTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -220,7 +220,7 @@ describe('Cli BAT test', function () {
   });
   it('Create a card from the new cardtype', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create card bat/templates/templateTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create card bat/templates/templateTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -235,7 +235,7 @@ describe('Cli BAT test', function () {
   });
   it('Add an attachment to a card', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create attachment ${newPageCardKey} ../tools/cli/test/cyberismo.png&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create attachment ${newPageCardKey} ../../tools/cli/test/cyberismo.png&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -249,7 +249,7 @@ describe('Cli BAT test', function () {
   });
   it('Add a link (of the new linktype) between two cards', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo create link ${pageCardKey} ${newPageCardKey} bat/linkTypes/linkTypeTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo create link ${pageCardKey} ${newPageCardKey} bat/linkTypes/linkTypeTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -263,7 +263,7 @@ describe('Cli BAT test', function () {
   });
   it('Move a card', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo move ${decisionCardKey} ${newPageCardKey}&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo move ${decisionCardKey} ${newPageCardKey}&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -277,7 +277,7 @@ describe('Cli BAT test', function () {
   });
   it('Change the rank of a card', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo rank card ${pageCardKey} ${newPageCardKey}&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo rank card ${pageCardKey} ${newPageCardKey}&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -291,7 +291,7 @@ describe('Cli BAT test', function () {
   });
   it('Export the static documentation site', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo export adoc ${newPageCardKey}&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo export adoc ${newPageCardKey}&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -305,7 +305,7 @@ describe('Cli BAT test', function () {
   });
   it('Generate calculations', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo calc generate&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo calc generate&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -319,7 +319,7 @@ describe('Cli BAT test', function () {
   });
   it('Remove the attachment', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo remove attachment ${newPageCardKey} cyberismo.png&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo remove attachment ${newPageCardKey} cyberismo.png&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -333,7 +333,7 @@ describe('Cli BAT test', function () {
   });
   it('Rename the project', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo rename cli&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo rename cli&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -352,7 +352,7 @@ describe('Cli BAT test', function () {
   });
   //  INTDEV-601
   //  it('Remove the link', function (done) {
-  //    exec(`cd ../../cyberismo-cli&&cyberismo remove link ${pageCardKey} ${newPageCardKey} cli/linkTypes/linkTypeTest&&cyberismo validate`, (error, stdout, _stderr) => {
+  //    exec(`cd ../../.tmp/cyberismo-cli&&cyberismo remove link ${pageCardKey} ${newPageCardKey} cli/linkTypes/linkTypeTest&&cyberismo validate`, (error, stdout, _stderr) => {
   //    if (error != null) {log(error);}
   //      log(error);
   //      log(stdout);
@@ -364,7 +364,7 @@ describe('Cli BAT test', function () {
   //  });
   it('Remove all cards of the new cardtype', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo remove card ${newPageCardKey}&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo remove card ${newPageCardKey}&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -379,7 +379,7 @@ describe('Cli BAT test', function () {
   });
   it('Remove the template', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo remove template cli/templates/templateTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo remove template cli/templates/templateTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
@@ -393,7 +393,7 @@ describe('Cli BAT test', function () {
   });
   //  INTDEV-602
   //  it('Remove the cardtype', function (done) {
-  //    exec(`cd ../../cyberismo-cli&&cyberismo remove cardtype cli/cardTypes/cardTypeTest&&cyberismo validate`, (error, stdout, _stderr) => {
+  //    exec(`cd ../../.tmp/cyberismo-cli&&cyberismo remove cardtype cli/cardTypes/cardTypeTest&&cyberismo validate`, (error, stdout, _stderr) => {
   //      if (error != null) {log(error);}
   //      expect(error).to.be.null;
   //      expect(stdout).to.include('Done');
@@ -403,7 +403,7 @@ describe('Cli BAT test', function () {
   //  });
   //  INTDEV-602
   //  it('Remove the workflow', function (done) {
-  //    exec(`cd ../../cyberismo-cli&&cyberismo remove workflow workflowTest&&cyberismo validate`, (error, stdout, _stderr) => {
+  //    exec(`cd ../../.tmp/cyberismo-cli&&cyberismo remove workflow workflowTest&&cyberismo validate`, (error, stdout, _stderr) => {
   //      if (error != null) {log(error);}
   //      expect(error).to.be.null;
   //      expect(stdout).to.include('Done');
@@ -413,7 +413,7 @@ describe('Cli BAT test', function () {
   //  });
   it('Remove the linktype', function (done) {
     exec(
-      `cd ../../cyberismo-cli&&cyberismo remove linkType cli/linkTypes/linkTypeTest&&cyberismo validate`,
+      `cd ../../.tmp/cyberismo-cli&&cyberismo remove linkType cli/linkTypes/linkTypeTest&&cyberismo validate`,
       (error, stdout, _stderr) => {
         if (error != null) {
           log(error);
