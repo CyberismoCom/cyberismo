aa745eb3cb0e7f13dacdbcc920944113062870dd
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-12-04 09:09:40 +0200

    INTDEV-595 do not try to collect fieldType information for predefined types

--- tools/data-handler/src/utils/constants.ts
+++ tools/data-handler/src/utils/constants.ts
@@ -1,1 +1,12 @@
 export const INT32_MAX = 2147483647; // 2^31-1
+/**
+ * These are names of the fields that are non-custom fields that present in metadata
+ */
+export const PREDEFINED_FIELDS = [
+  'rank',
+  'cardType',
+  'title',
+  'workflowState',
+  'lastUpdated',
+  'lastTransitioned',
+];
