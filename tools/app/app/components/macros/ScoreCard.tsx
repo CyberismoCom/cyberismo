b85f4fb811b24ffd6a9408f7e12fc60a9b015579
Jaakko Koskenkorva <jaakko@11latoa.com>
2024-12-05 12:59:17 +0200

    Use Joy UI for scoreCard macro in app

--- tools/app/app/components/macros/ScoreCard.tsx
+++ tools/app/app/components/macros/ScoreCard.tsx
@@ -10,6 +10,7 @@
     License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { Box, Card, Typography } from '@mui/joy';
 import { MacroContext } from '.';
 import React from 'react';
 
@@ -26,49 +27,52 @@ export default function ScoreCard({
   unit,
   legend,
 }: CreateCardsProps) {
-  const cardStyle = {
-    backgroundColor: '#ffffff',
-    border: '1px solid #e0e0e0',
-    borderRadius: '8px',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-    display: 'inline-block',
-    padding: '20px',
-    marginTop: '20px',
-    marginRight: '20px',
-  };
-
-  const titleStyle = {
-    fontSize: '16px',
-    color: '#666666',
-    margin: '0',
-    marginBottom: '4px',
-  };
-
-  const valueStyle = {
-    fontSize: '48px',
-    fontWeight: 'bold',
-    color: '#333333',
-    margin: '0',
-  };
-
-  const unitStyle = {
-    fontSize: '24px',
-  };
-
-  const legendStyle = {
-    fontSize: '14px',
-    color: '#999999',
-    marginTop: '4px',
-  };
-
   return (
-    <div style={cardStyle}>
-      <p style={titleStyle}>{title}</p>
-      <p style={valueStyle}>
-        {value}
-        <span style={unitStyle}>{unit}</span>
-      </p>
-      <p style={legendStyle}>{legend}</p>
-    </div>
+    <Card
+      variant="outlined"
+      style={{
+        borderRadius: '12px',
+        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+        display: 'inline-block',
+        padding: '20px',
+        marginTop: '20px',
+        marginRight: '20px',
+      }}
+    >
+      <Typography style={{ fontSize: '16px' }}>{title}</Typography>
+      <Box
+        style={{
+          display: 'flex',
+          alignItems: 'baseline',
+        }}
+      >
+        <Typography
+          style={{
+            fontSize: '48px',
+            fontWeight: 'bold',
+            color: '#333333',
+          }}
+        >
+          {value}
+        </Typography>
+        <Typography
+          style={{
+            fontSize: '24px',
+            marginLeft: '4px',
+            color: '#333333',
+          }}
+        >
+          {unit}
+        </Typography>
+      </Box>
+      <Typography
+        style={{
+          fontSize: '14px',
+          color: '#999999',
+        }}
+      >
+        {legend}
+      </Typography>
+    </Card>
   );
 }
