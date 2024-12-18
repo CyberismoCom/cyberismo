fc5654bd395fc7c42ce54f38bb7196bbf37f434c
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2024-12-18 09:44:20 +0200

    Revert "remove handlebars (#367)"
    This reverts commit dbd9067e971880a4db1b91f1759656ba7a959b1f.

--- tools/app/next.config.mjs
+++ tools/app/next.config.mjs
@@ -3,6 +3,9 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    config.resolve.alias.handlebars = path.resolve(
+      './node_modules/handlebars/dist/handlebars.js',
+    );
     config.externals.push({
       'thread-stream': 'commonjs thread-stream',
       pino: 'commonjs pino',
