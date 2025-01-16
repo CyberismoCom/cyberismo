caccdb52f8a8dca81c6509b03f56f92095418370
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2025-01-16 15:14:35 +0200

    Cleanup task queue

--- tools/data-handler/src/macros/task-queue.ts
+++ tools/data-handler/src/macros/task-queue.ts
@@ -1,5 +1,3 @@
-import { MacroTaskState } from '../interfaces/macros.js';
-
 /**
     Cyberismo
     Copyright © Cyberismo Ltd and contributors 2024
@@ -12,40 +10,58 @@ import { MacroTaskState } from '../interfaces/macros.js';
     License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { MacroTaskState } from '../interfaces/macros.js';
+
 export default class TaskQueue {
   private tasks: MacroTaskState[] = [];
 
-  public push(t: MacroTaskState) {
-    this.tasks.push(t);
+  /**
+   * Pushes a task to the task queue
+   * @param task The task to push
+   */
+  public push(task: MacroTaskState) {
+    this.tasks.push(task);
   }
 
+  /**
+   * Finds an existing task based on its id
+   * @param globalId unique id across both macro classes and instances
+   * @param localId Id within the instance
+   * @returns task if found else undefined
+   */
   public find(globalId: string, localId: number) {
     return this.tasks.find(
       (task) => task.globalId === globalId && task.localId === localId,
     );
   }
 
+  /**
+   * Waits for all tasks to be done
+   */
   public async waitAll() {
     for (const task of this.tasks) {
       await task.promise;
     }
   }
 
-  public async waitTask(t: MacroTaskState) {
-    await t.promise;
+  /**
+   * Waits for a single task to be done
+   * @param task task to wait
+   */
+  public async waitTask(task: MacroTaskState) {
+    await task.promise;
   }
 
-  public async wait(globalId: string, localId: number) {
-    const task = this.find(globalId, localId);
-    if (!task) {
-      throw new Error('Tried to wait a task that does not exist');
-    }
-    return this.waitTask(task);
-  }
-
+  /**
+   * Removes all tasks. If they are running, they continue to run because they are promises
+   */
   public async reset() {
     this.tasks = [];
   }
+
+  /**
+   * When iterating over task queue, it'll return each task
+   */
   public [Symbol.iterator]() {
     let index = 0;
     const tasks = this.tasks;
