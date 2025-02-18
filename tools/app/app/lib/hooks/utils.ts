c9b8bd04fd52e940f2445f12d086f54645b1e1e9
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2025-02-18 12:36:04 +0200

    useResizeObserver: Support all browsers (#502)
    * useResizeObserver: Support all browsers
    
    * resizeobserver: wait next animation frame

--- tools/app/app/lib/hooks/utils.ts
+++ tools/app/app/lib/hooks/utils.ts
@@ -43,6 +43,29 @@ export function createFunctionGuard<T extends unknown[], U>(
     }
   };
 }
+
+/**
+ * This function is used to get the value of a ResizeObserverEntry.
+ * @param entry - the ResizeObserverEntry
+ * @param value - the value to get
+ * @returns the value of the ResizeObserverEntry
+ */
+function getEntryValue(entry: ResizeObserverEntry, value: 'width' | 'height') {
+  const { contentRect, contentBoxSize } = entry;
+
+  if (!contentBoxSize) {
+    if (!contentRect) {
+      return 0;
+    }
+    return contentRect[value];
+  }
+
+  const accessor = value === 'width' ? 'inlineSize' : 'blockSize';
+  return contentBoxSize[0]
+    ? contentBoxSize[0][accessor]
+    : // @ts-ignore
+      contentBoxSize[accessor];
+}
 /**
  * This hook is used to observe the size of a DOM element.
  * @returns the width and height of the element and a ref to be attached to the element
@@ -59,10 +82,12 @@ export function useResizeObserver() {
     if (!node) return;
 
     const observer = new ResizeObserver((entries) => {
-      const entry = entries[0];
-      setDimensions({
-        width: entry.contentRect.width,
-        height: entry.contentRect.height,
+      requestAnimationFrame(() => {
+        const entry = entries[0];
+        setDimensions({
+          width: getEntryValue(entry, 'width'),
+          height: getEntryValue(entry, 'height'),
+        });
       });
     });
 
