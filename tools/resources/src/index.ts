47fee5e7440fe01fddc0e8426f2c73fa0f7c1c4c
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2025-05-26 14:15:45 +0300

    Create resources package

---
+++ tools/resources/src/index.ts
@@ -0,0 +1,44 @@
+export * from './schemas.js';
+
+// Manually import each resource
+// They rarely change and we get 100% type safety this way
+import commonBase from './calculations/common/base.lp';
+import commonQueryLanguage from './calculations/common/queryLanguage.lp';
+import queriesCard from './calculations/queries/card.lp';
+import queriesOnCreation from './calculations/queries/onCreation.lp';
+import queriesOnTransition from './calculations/queries/onTransition.lp';
+import queriesTree from './calculations/queries/tree.lp';
+import testModel from './calculations/test/model.lp';
+
+export const lpFiles = {
+  common: {
+    base: commonBase,
+    queryLanguage: commonQueryLanguage,
+  },
+  queries: {
+    card: queriesCard,
+    onCreation: queriesOnCreation,
+    onTransition: queriesOnTransition,
+    tree: queriesTree,
+  },
+  test: {
+    model: testModel,
+  },
+};
+
+// Helper function to get the absolute path to the static directory
+export async function getStaticDirectoryPath(): Promise<string> {
+  // Return early if not running in Node.js
+  if (typeof process === 'undefined' || !process.versions?.node) {
+    throw new Error(
+      '@cyberismocom/resources: getStaticDirectoryPath can only be called in Node.js',
+    );
+  }
+  const { join } = await import('node:path');
+
+  // Get the directory of the current module
+  const currentModuleDir = import.meta.dirname;
+
+  // Look for static directory in the package
+  return join(currentModuleDir, 'static');
+}
