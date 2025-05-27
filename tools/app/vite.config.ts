a000dd887adb4e389f6a8220873ed510a2940466
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2025-05-27 14:07:18 +0300

    WIP App

--- tools/app/vite.config.ts
+++ tools/app/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import license from 'rollup-plugin-license';
 import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    license({
+      thirdParty: {
+        output: {
+          file: path.join(__dirname, 'dist', 'THIRD-PARTY.txt'),
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
