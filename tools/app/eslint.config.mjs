16d668735278191569399bd41b9ec404d77597c9
Sami Merilä <sami.merila@cyberismo.com>
2025-06-02 10:25:59 +0300

    Do not lint coverage test results

--- tools/app/eslint.config.mjs
+++ tools/app/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'coverage'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
