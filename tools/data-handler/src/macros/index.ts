8740427df0a62751ed01e47548d2da18a218b977
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2025-07-18 13:45:12 +0300

    Match outer most raw macros

--- tools/data-handler/src/macros/index.ts
+++ tools/data-handler/src/macros/index.ts
@@ -39,8 +39,7 @@ const CURLY_RIGHT = '&#125;';
  * @returns The processed content with raw blocks escaped
  */
 function preprocessRawBlocks(content: string): string {
-  // Find all {{#raw}}...{{/raw}} blocks
-  const rawBlockRegex = /\{\{#raw\}\}([\s\S]*?)\{\{\/raw\}\}/g;
+  const rawBlockRegex = /\{\{#raw\}\}([\s\S]*)\{\{\/raw\}\}/g;
 
   return content.replace(rawBlockRegex, (_, innerContent) => {
     // Escape all curly braces in the inner content
