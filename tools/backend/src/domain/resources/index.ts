8db4fbf7241fa9b45df9d11c9a9a10040c211c9c
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2025-08-27 10:26:44 +0300

    Remove unused import

--- tools/backend/src/domain/resources/index.ts
+++ tools/backend/src/domain/resources/index.ts
@@ -13,10 +13,7 @@
 
 import { Hono } from 'hono';
 import * as resourceService from './service.js';
-import type {
-  ResourceFileContentResponse,
-  ResourceValidationResponse,
-} from '../../types.js';
+import type { ResourceFileContentResponse } from '../../types.js';
 import { resourceParamsSchema } from '../../common/validationSchemas.js';
 import { zValidator } from '../../middleware/zvalidator.js';
 import { validateResourceParamsSchema } from './schema.js';
