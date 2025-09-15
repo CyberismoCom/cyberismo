4d6fd6632cd0e83ce58c95ceaf8c839147b9cb3f
Sami Merilä <sami.merila@cyberismo.com>
2025-09-15 14:10:57 +0300

    Remove stderr about invalid JSON (#906)

--- tools/cli/src/index.ts
+++ tools/cli/src/index.ts
@@ -670,7 +670,7 @@ importCmd
             options.projectPath,
           );
           const moduleListPath = resolve(projectPath, '.temp/moduleList.json');
-          let moduleListContent = '';
+          let moduleListContent = '{ modules: [] }';
           try {
             moduleListContent = await readFile(moduleListPath, 'utf-8');
           } catch {
