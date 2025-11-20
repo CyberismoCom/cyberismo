8bc3abf7ccc6335d2f4ea2152abd3de69131d920
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2025-11-20 13:56:11 +0200

    Fix prettier

--- tools/app/src/components/toolbar/ConfigToolbar.tsx
+++ tools/app/src/components/toolbar/ConfigToolbar.tsx
@@ -109,10 +109,15 @@ export function ConfigToolbar({
   return (
     <BaseToolbar
       breadcrumbs={breadcrumbs}
-      contextMenu={<ConfigContextMenu node={node} enabled={{
-        ...enabled,
-        delete: enabled?.delete && !node.readOnly
-      }} />}
+      contextMenu={
+        <ConfigContextMenu
+          node={node}
+          enabled={{
+            ...enabled,
+            delete: enabled?.delete && !node.readOnly,
+          }}
+        />
+      }
       actions={actions}
     />
   );
