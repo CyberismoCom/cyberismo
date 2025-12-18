09c803381062ef0edbc828e55bb0069b1d4b51ca
Sami Merilä <sami.merila@cyberismo.com>
2025-12-18 12:01:39 +0200

    INTDEV-1139 Show which file is problematic when having invalid JSON

--- tools/data-handler/src/utils/json.ts
+++ tools/data-handler/src/utils/json.ts
@@ -28,9 +28,7 @@ export function readJsonFileSync(file: string) {
     return returnValue;
   } catch (error) {
     if (error instanceof Error) {
-      throw new Error(
-        `Error while handling JSON file '${file}' : ${error.message}`,
-      );
+      throw new Error(`Invalid JSON in file '${file}': ${error.message}`);
     }
   }
 }
@@ -47,9 +45,7 @@ export async function readJsonFile(file: string) {
     return JSON.parse(raw);
   } catch (error) {
     if (error instanceof Error) {
-      throw new Error(
-        `Error while handling JSON file '${file}' : ${error.message}`,
-      );
+      throw new Error(`Invalid JSON in file '${file}': ${error.message}`);
     }
   }
 }
