46b60b9f5470f61a9321ac13530986213b3e40f0
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2026-01-14 10:18:58 +0200

    Do not update license year in automatically generated file (#1142)

--- tools/assets/scripts/generateSchemaImports.js
+++ tools/assets/scripts/generateSchemaImports.js
@@ -8,7 +8,7 @@ const parentSchemaFile = 'cardTreeDirectorySchema';
 const license = `
 /**
   Cyberismo
-  Copyright © Cyberismo Ltd and contributors ${new Date().getFullYear()}
+  Copyright © Cyberismo Ltd and contributors 2025
   This program is free software: you can redistribute it and/or modify it under
   the terms of the GNU Affero General Public License version 3 as published by
   the Free Software Foundation.
