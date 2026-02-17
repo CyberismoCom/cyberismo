fc278670f2f0f9136945c6b067d5093e4e8caedd
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2026-02-17 14:16:26 +0200

    Increase timeout

--- tools/app/cypress.config.ts
+++ tools/app/cypress.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     experimentalMemoryManagement: true,
+    taskTimeout: 120000,
     setupNodeEvents(on) {
       on('task', {
         deleteTestProject() {
