ad04e147f86cff99203f6291ae2c3c515a701ef6
adam-kuhn <adam.kuhn20@gmail.com>
2026-03-20 10:09:51 +0200

    common-utils, mocha to vitest

--- tools/data-handler/test/utils/common-utils.test.ts
+++ tools/data-handler/test/utils/common-utils.test.ts
@@ -1,5 +1,4 @@
-// testing
-import { expect } from 'chai';
+import { expect, describe, it } from 'vitest';
 
 import { deepCompare } from '../../src/utils/common-utils.js';
 
@@ -9,41 +8,41 @@ describe('common utils', () => {
       const obj1 = { key: 'value' };
       const obj2 = { anotherKey: 'value' };
       const result = deepCompare(obj1, obj2);
-      expect(result).to.equal(false);
+      expect(result).toBe(false);
     });
     it('should indicate that same objects, but different values are different', () => {
       const obj1 = { key: 'value' };
       const obj2 = { key: 'value2' };
       const result = deepCompare(obj1, obj2);
-      expect(result).to.equal(false);
+      expect(result).toBe(false);
     });
     it('should indicate that same objects are same', () => {
       const obj1 = { key: 'value' };
       const obj2 = { key: 'value' };
       const result = deepCompare(obj1, obj2);
-      expect(result).to.equal(true);
+      expect(result).toBe(true);
     });
     it('should indicate that when comparing object and array they are different', () => {
       const obj1 = { key: 'value' };
       const arr = [{ key: 'value' }];
       const result = deepCompare(obj1, arr);
-      expect(result).to.equal(false);
+      expect(result).toBe(false);
     });
     it('should indicate that same arrays are same', () => {
       const arr1 = [{ key: 'value' }];
       const arr2 = [{ key: 'value' }];
       const result = deepCompare(arr1, arr2);
-      expect(result).to.equal(true);
+      expect(result).toBe(true);
     });
     it('should indicate that different arrays are different', () => {
       const arr1 = [{ key: 'value' }];
       const arr2 = [{ key: 'anotherValue' }];
       const result = deepCompare(arr1, arr2);
-      expect(result).to.equal(false);
+      expect(result).toBe(false);
     });
     it('should indicate that empty objects are same', () => {
       const result = deepCompare({}, {});
-      expect(result).to.equal(true);
+      expect(result).toBe(true);
     });
   });
 });
