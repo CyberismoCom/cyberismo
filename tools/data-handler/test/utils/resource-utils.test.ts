51ac537507a7b93d19c338d620ced070c6e4005c
adam-kuhn <adam.kuhn20@gmail.com>
2026-03-20 12:51:25 +0200

    resource-utils from mocha to vitest

--- tools/data-handler/test/utils/resource-utils.test.ts
+++ tools/data-handler/test/utils/resource-utils.test.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { expect, it, describe, beforeAll } from 'vitest';
 
 import { join, sep } from 'node:path';
 
@@ -14,91 +14,88 @@ import type { Project } from '../../src/containers/project.js';
 import { getTestProject } from '../helpers/test-utils.js';
 
 describe('resource utils', () => {
-  it('resourceName with valid resource name (success)', () => {
-    const name = 'test/test/test';
-    // note that resource name util does not handle incorrect prefixes, or types
-    const { identifier, prefix, type } = resourceName(name);
-    expect(prefix).to.equal('test');
-    expect(type).to.equal('test');
-    expect(identifier).to.equal('test');
-  });
-  it('resourceName with valid identifier (success)', () => {
-    const name = 'test';
-    const { identifier, prefix, type } = resourceName(name);
-    expect(prefix).to.equal('');
-    expect(type).to.equal('');
-    expect(identifier).to.equal('test');
-  });
-  it('resourceName with only identifier and using "strict" throws', () => {
-    const name = 'test';
-    const strictNameValidation = true;
-    expect(() => resourceName(name, strictNameValidation)).to.throw(
-      "Name 'test' is not valid resource name",
+  describe('resourceName', () => {
+    it('validates a valid resource name (success)', () => {
+      const name = 'test/test/test';
+      // note that resource name util does not handle incorrect prefixes, or types
+      const { identifier, prefix, type } = resourceName(name);
+      expect(prefix).toBe('test');
+      expect(type).toBe('test');
+      expect(identifier).toBe('test');
+    });
+    it('validates a valid identifier (success)', () => {
+      const name = 'test';
+      const { identifier, prefix, type } = resourceName(name);
+      expect(prefix).toBe('');
+      expect(type).toBe('');
+      expect(identifier).toBe('test');
+    });
+    it('throws an error when doing strict identifier validation', () => {
+      const name = 'test';
+      const strictNameValidation = true;
+      expect(() => resourceName(name, strictNameValidation)).toThrow(
+        "Name 'test' is not valid resource name",
+      );
+    });
+    it.each(['', 'test/test', 'test/test/test/test'])(
+      'throws when resource name is invalid - %s',
+      (invalidName) => {
+        expect(() => resourceName(invalidName)).toThrow();
+      },
     );
   });
-  it('resourceName with invalid names', () => {
-    const invalidResourceNames = ['', 'test/test', 'test/test/test/test'];
-    for (const invalidName of invalidResourceNames) {
+  describe('resourceNameToString', () => {
+    it('returns the resource name when provided a valid ResourceName', () => {
+      const resourceName: ResourceName = {
+        prefix: 'test',
+        type: 'test',
+        identifier: 'test',
+      };
+      const stringName = resourceNameToString(resourceName);
+      expect(stringName).toBe('test/test/test');
+    });
+    it('returns the identifies when that is the only resource property defined', () => {
+      const resourceName: ResourceName = {
+        prefix: '',
+        type: '',
+        identifier: 'test',
+      };
+      const stringName = resourceNameToString(resourceName);
+      expect(stringName).toBe('test');
+    });
+    it('returns an empty string when all resource properties are empty', () => {
+      const resourceName: ResourceName = {
+        prefix: '',
+        type: '',
+        identifier: '',
+      };
+      const stringName = resourceNameToString(resourceName);
+      expect(stringName).toBe('');
+    });
+    it.each([
+      [{ prefix: 'a', type: '', identifier: 'a' }],
+      [{ prefix: 'a', type: '', identifier: '' }],
+      [{ prefix: '', type: 'a', identifier: '' }],
+      [{ prefix: '', type: 'a', identifier: 'a' }],
+    ])('throws an error when resources have invalid values', (resourceName) => {
       expect(() => {
-        resourceName(invalidName);
-      }).to.throw();
-    }
-  });
-  it('resourceNameToString with valid resource name', () => {
-    const resourceName: ResourceName = {
-      prefix: 'test',
-      type: 'test',
-      identifier: 'test',
-    };
-    const stringName = resourceNameToString(resourceName);
-    expect(stringName).to.equal('test/test/test');
-  });
-  it('resourceNameToString with valid identifier', () => {
-    const resourceName: ResourceName = {
-      prefix: '',
-      type: '',
-      identifier: 'test',
-    };
-    const stringName = resourceNameToString(resourceName);
-    expect(stringName).to.equal('test');
-  });
-  it('resourceNameToString with empty string', () => {
-    const resourceName: ResourceName = {
-      prefix: '',
-      type: '',
-      identifier: '',
-    };
-    const stringName = resourceNameToString(resourceName);
-    expect(stringName).to.equal('');
-  });
-  it('resourceNameToString with invalid values', () => {
-    const invalidResourceNames: ResourceName[] = [
-      { prefix: 'a', type: '', identifier: 'a' },
-      { prefix: 'a', type: '', identifier: '' },
-      { prefix: '', type: 'a', identifier: '' },
-      { prefix: '', type: 'a', identifier: 'a' },
-    ];
-    for (const invalidName of invalidResourceNames) {
-      expect(() => {
-        resourceNameToString(invalidName);
-      }).to.throw();
-    }
+        resourceNameToString(resourceName);
+      }).toThrow();
+    });
   });
 });
 
-describe('resource utils with Project instance', () => {
+describe('resource utils with Project instance', async () => {
   const baseDir = import.meta.dirname;
   const decisionRecordsPath = join(
     baseDir,
     '../test-data/valid/decision-records',
   );
-  let project: Project;
-  before(async () => {
-    project = getTestProject(decisionRecordsPath);
-    await project.populateCaches();
-  });
-  it('resourceNameToPath with valid values', () => {
-    const validNames: Map<ResourceName, string> = new Map([
+  const project = getTestProject(decisionRecordsPath);
+  await project.populateCaches();
+
+  describe('resourceNameToPath', () => {
+    it.each([
       [
         {
           prefix: project.projectPrefix,
@@ -115,64 +112,37 @@ describe('resource utils with Project instance', () => {
         },
         `${project.paths.resourcesFolder}${sep}workflows${sep}decision.json`,
       ],
-    ]);
-
-    for (const name of validNames) {
-      const resultPath = resourceNameToPath(project, name[0]);
-      expect(resultPath).to.equal(name[1]);
-    }
-  });
-  it('resourceNameToPath with custom extension', () => {
-    const resultPath = resourceNameToPath(
-      project,
-      {
-        prefix: project.projectPrefix,
-        type: 'calculations',
-        identifier: 'test',
-      },
-      '.lp',
-    );
-    expect(resultPath).to.equal(
-      `${project.paths.resourcesFolder}${sep}calculations${sep}test.lp`,
-    );
-  });
-  it('resourceNameToPath with empty prefix throws', () => {
-    expect(() =>
-      resourceNameToPath(project, {
-        prefix: '',
-        type: '',
-        identifier: '',
-      }),
-    ).to.throw('resourceName does not contain prefix');
-  });
-
-  it('resourceNameToPath with extension', () => {
-    const validNames: Map<string, ResourceName> = new Map([
-      [
-        `${project.paths.resourcesFolder}${sep}test${sep}test.test`,
+    ])('converts the resource name to path', (resourceName, expectedPath) => {
+      const resultPath = resourceNameToPath(project, resourceName);
+      expect(resultPath).toBe(expectedPath);
+    });
+    it('converts ResourceName to path with a custom extension', () => {
+      const resultPath = resourceNameToPath(
+        project,
         {
           prefix: project.projectPrefix,
-          type: 'test',
+          type: 'calculations',
           identifier: 'test',
         },
-      ],
-      [
-        `${project.paths.modulesFolder}${sep}base${sep}test${sep}test.test`,
-        {
-          prefix: 'base',
-          type: 'test',
-          identifier: 'test',
-        },
-      ],
-    ]);
-    for (const name of validNames) {
-      const filePath = resourceNameToPath(project, name[1], '.test');
-      expect(filePath).to.equal(name[0]);
-    }
+        '.lp',
+      );
+      expect(resultPath).toBe(
+        `${project.paths.resourcesFolder}${sep}calculations${sep}test.lp`,
+      );
+    });
+    it('throws an error if ResourceName has an empty prefix', () => {
+      expect(() =>
+        resourceNameToPath(project, {
+          prefix: '',
+          type: '',
+          identifier: '',
+        }),
+      ).toThrow('resourceName does not contain prefix');
+    });
   });
 
-  it('pathToResourceName with valid values', () => {
-    const validNames: Map<string, ResourceName> = new Map([
+  describe('pathToResourceName', () => {
+    it.each([
       [
         `${project.paths.resourcesFolder}${sep}cardTypes${sep}test.json`,
         {
@@ -197,16 +167,14 @@ describe('resource utils with Project instance', () => {
           identifier: 'decision',
         },
       ],
-    ]);
-    for (const name of validNames) {
-      const resourceName = pathToResourceName(project, name[0]);
-      expect(resourceName.prefix).to.equal(name[1].prefix);
-      expect(resourceName.type).to.equal(name[1].type);
-      expect(resourceName.identifier).to.equal(name[1].identifier);
-    }
-  });
-  it('pathToResourceName with invalid values', () => {
-    const invalidNames: Map<string, string> = new Map([
+    ])('converts path to ResourceName', (path, expectedResourceName) => {
+      const resourceName = pathToResourceName(project, path);
+      expect(resourceName.prefix).toBe(expectedResourceName.prefix);
+      expect(resourceName.type).toBe(expectedResourceName.type);
+      expect(resourceName.identifier).toBe(expectedResourceName.identifier);
+    });
+
+    it.each([
       [
         `${project.paths.resourcesFolder}${sep}cardTypes${sep}`,
         `invalid path:`,
@@ -219,18 +187,18 @@ describe('resource utils with Project instance', () => {
         `${project.paths.resourcesFolder}${sep}base${sep}workflows${sep}decision.json`,
         'not a resource path:',
       ],
-    ]);
-    for (const name of invalidNames) {
-      expect(() => pathToResourceName(project, name[0])).to.throw(
-        `${name[1]} ${name[0]}`,
+    ])('throws an error when path is invalid', (path, expectedErrorMessage) => {
+      expect(() => pathToResourceName(project, path)).toThrow(
+        `${expectedErrorMessage} ${path}`,
       );
-    }
+    });
   });
 
-  it('resourceFilePath with extension', () => {
-    const validNames: Map<string, ResourceName> = new Map([
+  describe('resourceFilePath', () => {
+    const extension = 'fileName.extension';
+    it.each([
       [
-        `${project.paths.resourcesFolder}${sep}type${sep}identifier${sep}fileName.extension`,
+        `${project.paths.resourcesFolder}${sep}type${sep}identifier${sep}${extension}`,
         {
           prefix: project.projectPrefix,
           type: 'type',
@@ -238,17 +206,19 @@ describe('resource utils with Project instance', () => {
         },
       ],
       [
-        `${project.paths.modulesFolder}${sep}base${sep}type${sep}identifier${sep}fileName.extension`,
+        `${project.paths.modulesFolder}${sep}base${sep}type${sep}identifier${sep}${extension}`,
         {
           prefix: 'base',
           type: 'type',
           identifier: 'identifier',
         },
       ],
-    ]);
-    for (const name of validNames) {
-      const filePath = resourceFilePath(project, name[1], 'fileName.extension');
-      expect(filePath).to.equal(name[0]);
-    }
+    ])(
+      'handles the file path correctly when an extension is provided',
+      (expectedPath, resourceName) => {
+        const filePath = resourceFilePath(project, resourceName, extension);
+        expect(filePath).toBe(expectedPath);
+      },
+    );
   });
 });
