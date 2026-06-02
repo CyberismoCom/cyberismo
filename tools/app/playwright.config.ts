1d3351e771d488d82fdd9289335de26241755ee1
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2026-06-02 11:32:21 +0300

    add html report to CI

--- tools/app/playwright.config.ts
+++ tools/app/playwright.config.ts
@@ -21,7 +21,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 2 : undefined,
-  reporter: process.env.CI ? [['list'], ['github']] : 'list',
+  reporter: process.env.CI
+    ? [['list'], ['github'], ['html', { open: 'never' }]]
+    : 'list',
   timeout: 60_000,
   expect: { timeout: 10_000 },
   use: {
