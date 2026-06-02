57d9bcfd07d2c382a39bda326fe7eaff02f5ffce
Samu Toljamo <samu.toljamo@paretosoftware.fi>
2026-06-02 13:21:39 +0000

    Fix license

--- tools/data-handler/src/mutations/dispatcher.ts
+++ tools/data-handler/src/mutations/dispatcher.ts
@@ -1,4 +1,16 @@
-// tools/data-handler/src/mutations/dispatcher.ts
+/**
+  Cyberismo
+  Copyright © Cyberismo Ltd and contributors 2026
+
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU Affero General Public License version 3 as published by
+  the Free Software Foundation. This program is distributed in the hope that it
+  will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Affero General Public License for more details.
+  You should have received a copy of the GNU Affero General Public
+  License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 
 import type { Handler, MutationContext } from './handler.js';
 import { DefaultNoCascadeHandler } from './handlers/default-no-cascade.js';
